3d26cbb5f9bc1a1af9ad6284f56ba29f45540547
xiboio <murilodaniel02@gmail.com>
2026-03-04 13:05:09 -0300

    create connection with sql

--- server.js
+++ server.js
@@ -11,3 +11,20 @@ app.listen(3001, () => {
     console.log("Servidor iniciado na porta 3001")
 })
 
+const mysql = require("mysql2")
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "murilo02",
+    database: "oficina"
+})
+
+connerction.connection((err) =>{
+    if (err) {
+        console.error("Erro aoi conectar", err)
+        return
+    }
+    console.log("Conectado ao MySql")
+})
+
