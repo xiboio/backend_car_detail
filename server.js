1943ad9da8536840a55395566cca557ae4ac94e1
xiboio <murilodaniel02@gmail.com>
2026-03-11 21:35:11 -0300

    update

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const mysql = require("mysql2")
+const cors = require("cors")
 
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 app.get("/", (req, res) => {
@@ -29,6 +31,9 @@ connection.connect((err) =>{
 })
 
 app.post("/atendimentos", (req, res) => {
+
+    console.log("Rota foi chamada")
+
     const {
         data,
         cpf,
